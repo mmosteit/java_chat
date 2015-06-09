091c28724afd972a8302af42cb3c452532279815
Michael Mosteit <mmosteit@gmail.com>
2015-06-08 20:31:30 -0400

    Added error handling to initial startup dialog

--- src/chat/MainProgram.java
+++ src/chat/MainProgram.java
@@ -18,83 +18,90 @@ public class MainProgram {
 
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run(){
-
-				StartupDialog startup;
-				startup = new StartupDialog();
-				Vector<String> retVal;
-				retVal = startup.getResult();
-				if(retVal == null){
-					System.out.println("NULL retval");
-				}
-				else{
-					System.out.println(retVal.toString());
-
-					if (retVal.get(0).equals("Client")){
-
-						if (retVal.get(1).trim().equals("") || retVal.get(2).trim().equals("") || retVal.get(3).trim().equals("") ){
-
-							JOptionPane.showMessageDialog(null,"Server IP address, roomname and username must be specified for client");
-						}
-						else{
-							String ip_addr  = retVal.get(1);
-							String chatroom = retVal.get(2);
-							String username = retVal.get(3);
-							Thread ConnectionThread;
-							ClientGui client = new ClientGui(username);
-							ClientGuiConnection connection;
-							try {
-								connection = new ClientGuiConnection(client,ip_addr, chatroom, username);
-								client.SetConnection(connection);
-								ConnectionThread = new Thread(connection);
-								ConnectionThread.start();
-								client.setTitle(username+"@"+chatroom);
-							} 
-							catch(IOException  e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							catch(RoomNameException e){
-								e.printStackTrace();
-							}
-							catch(UserNameException e){
-								e.printStackTrace();
-							}
-
-						}
-
-					}
-					else if(retVal.get(0).equals("Server")){
-						if(retVal.get(1).trim().equals("") || retVal.get(2).trim().equals("")){
-							JOptionPane.showMessageDialog(null,"Error: Server IP address and chatroom names must be specified for server");
-						}
-						else{
-
-							MainServer server;
-							Vector<String> args = new Vector<String>();
-							args.add("server"); // Technically does not matter
-							args.add(retVal.get(1));
-							String [] roomNames;
-							roomNames = retVal.get(2).split("\\s+");// Split the roomnames string into an array by whitespace
-							Collections.addAll(args, roomNames);
-
-							try {
-								server = new MainServer(args);
-								server.run();
-							} catch (BindException e){
-								JOptionPane.showMessageDialog(null, "Error: Could not bind to "+retVal.get(1));
-							} catch (IOException e){
-								JOptionPane.showMessageDialog(null, "Constructor failed due to IOException ");
-							} catch (Exception e){
-								JOptionPane.showMessageDialog(null,"Caught a generic exception from mainserver in main method:"+e.toString());
-							}
-						}
-
-
+				boolean done = false;
+				while(done == false){
+					StartupDialog startup;
+					startup = new StartupDialog();
+					Vector<String> retVal;
+					retVal = startup.getResult();
+					if(retVal == null){ // The user closed the startup dialog
+						System.exit(0);
 					}
 					else{
-						System.out.println("Code should not get here in MainProgram.java");
-					}
+						
 
+						if (retVal.get(0).equals("Client")){
+
+							if (retVal.get(1).trim().equals("") || retVal.get(2).trim().equals("") || retVal.get(3).trim().equals("") ){
+
+								JOptionPane.showMessageDialog(null,"Server IP address, roomname and username must be specified for client");
+							}
+							else{
+								String ip_addr  = retVal.get(1);
+								String chatroom = retVal.get(2);
+								String username = retVal.get(3);
+								Thread ConnectionThread;
+								ClientGui client = new ClientGui(username);
+								ClientGuiConnection connection;
+								try {
+									connection = new ClientGuiConnection(client,ip_addr, chatroom, username);
+									client.SetConnection(connection);
+									ConnectionThread = new Thread(connection);
+									ConnectionThread.start();
+									client.setTitle(username+"@"+chatroom);
+									done = true;
+								} 
+								catch(IOException  e) {
+									// TODO Auto-generated catch block
+									client.dispose();
+									JOptionPane.showMessageDialog(null, "Error: Could not bind to "+retVal.get(1));
+								}
+								catch(RoomNameException e){
+									client.dispose();
+									JOptionPane.showMessageDialog(null, "Error: No room named "+chatroom);
+								}
+								catch(UserNameException e){
+									client.dispose();
+									JOptionPane.showMessageDialog(null, "Error: Selecter username already in use.");
+								}
+
+							}
+
+						}
+						else if(retVal.get(0).equals("Server")){
+							if(retVal.get(1).trim().equals("") || retVal.get(2).trim().equals("")){
+								JOptionPane.showMessageDialog(null,"Error: Server IP address and chatroom names must be specified for server");
+							}
+							else{
+
+								MainServer server;
+								Vector<String> args = new Vector<String>();
+								args.add("server"); // Technically does not matter
+								args.add(retVal.get(1));
+								String [] roomNames;
+								roomNames = retVal.get(2).split("\\s+");// Split the roomnames string into an array by whitespace
+								Collections.addAll(args, roomNames);
+
+								try {
+									server = new MainServer(args);
+									server.run();
+									done = true;
+								} catch (BindException e){
+									JOptionPane.showMessageDialog(null, "Error: Could not bind to "+retVal.get(1));
+								} catch (IOException e){
+									JOptionPane.showMessageDialog(null, "Constructor failed due to IOException ");
+								} catch (Exception e){
+									JOptionPane.showMessageDialog(null,"Caught a generic exception from mainserver in main method:"+e.toString());
+								}
+							}
+
+
+						}
+						else{
+							System.out.println("Code should not get here in MainProgram.java");
+						}
+
+					}
 				}
 			}
 		});
